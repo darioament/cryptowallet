12ad0f58a96e8644de5d4ba2562ec4b50fcbd58d
darioament <ament.dario@gmail.com>
2025-12-04 19:38:59 +0100

    import adjusted

--- composeApp/src/commonMain/kotlin/fina/dario/cryptowallet/App.kt
+++ composeApp/src/commonMain/kotlin/fina/dario/cryptowallet/App.kt
@@ -1,23 +1,7 @@
 package fina.dario.cryptowallet
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import fina.dario.cryptowallet.generated.resources.Res
-import fina.dario.cryptowallet.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
